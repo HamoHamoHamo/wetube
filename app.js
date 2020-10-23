1185ce0e53b4952b0e37b96cbacceadaa2bbe1dc
HamoHamoHamo <hamo222@naver.com>
2020-10-23 23:27:34 +0900

    #2.8 Express Core: Routing - use router

--- app.js
+++ app.js
@@ -3,21 +3,12 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () =>
-    console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from homeaa");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
-
-const betweenHome = (req, res, next) => {
-    console.log('Between');
-    next();
-};
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -25,9 +16,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(4000, handleListening);
+app.use("/user", userRouter);
+export default app;
