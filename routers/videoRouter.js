228e2ca79de94bd4d482c1182c123ab3aeaaf589
HamoHamoHamo <hamo222@naver.com>
2020-11-15 14:03:36 +0000

    #3.9 Editing a Video - change title and description

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,15 +1,20 @@
 import express from "express";
-import { deleteVideo, editVideo, getUpload, postUpload, videoDetail, videos } from "../controllers/videoController";
+import { deleteVideo, getUpload, postUpload, videoDetail, videos, getEditVideo, postEditVideo } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 import routes from "../routes";
 
 const videoRouter = express.Router();
 
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
