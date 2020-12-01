66f99a4dd89b91f6072d32af113365e85774a50f
HamoHamoHamo <hamo222@naver.com>
2020-12-01 14:52:34 +0000

    #6.6 Github Log In part Two

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,10 +34,18 @@ export const postLogin = passport.authenticate("local", {
     successRedirect: routes.home
 });
 
+export const githubLoginCallback = (accessToken, refreshToken, profile, cb) => {
+    console.log(accessToken, refreshToken, profile, cb);
+};
+
+export const postGithubLogIn = (req, res) => {
+    res.send(routes.home);
+}
+
+export const githubLogin = passport.authenticate("github");
 
 export const logout = (req, res) => {
-    // To Do: Process Log Out
-
+    req.logout();
     res.redirect(routes.home);
 };
 
